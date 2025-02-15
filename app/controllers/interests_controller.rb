1c428956a7546ada5e1f8ccd14a7290a18c18b2a
fernandodxx <fernandosillvasantoss@gmail.com>
2025-02-15 09:31:49 -0300

    Interest create

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -3,4 +3,19 @@ class InterestsController < ApplicationController
     @interests = Interest.all
     render json: @interests
   end
+
+  def create
+    @interest = Interest.new(interest_params)
+    if @interest.save
+      render json: @interest, status: :created
+    else
+      render json: @interest.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def interest_params
+    params.expect(interest: [ :name ])
+  end
 end

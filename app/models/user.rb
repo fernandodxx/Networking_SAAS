bcb543b3e78863005551c46590caa4f04c3b0aeb
fernandodxx <fernandosillvasantoss@gmail.com>
2025-02-15 09:01:56 -0300

    User Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_interests
+  has_many :interests, through: :user_interests
+
+  has_many :connections
+  has_many :connected_users, through: :connections, source: :connected_user
 end

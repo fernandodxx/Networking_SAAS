5670f03c0183facb5de2b2b2500bd2bcd8eb08fa
fernandodxx <fernandosillvasantoss@gmail.com>
2025-02-15 09:25:57 -0300

    User create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     render json: @user
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.expect(user: [ :name, :email ])
+  end
 end

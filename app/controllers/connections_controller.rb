be3cd5224f11099e674e0fd8095775ef178ffbf2
fernandodxx <fernandosillvasantoss@gmail.com>
2025-02-15 09:36:18 -0300

    Connection  create

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -3,4 +3,19 @@ class ConnectionsController < ApplicationController
     @connections = Connection.all
     render json: @connections
   end
+
+  def create
+    @connection = Connection.new(connection_params)
+    if @connection.save
+      render json: @connection, status: :created
+    else
+      render json: @connection.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def connection_params
+    params.expect(connection: [ :user_id, :connected_user_id ])
+  end
 end
